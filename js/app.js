b349aada8dfa0ba15dc894ae71a29057048bdbac
daisha.mays <daishamays@gmail.com>
2022-03-25 00:59:45 -0400

    Update

--- js/app.js
+++ js/app.js
@@ -1,15 +1,23 @@
-console.log("Java is working")
 
+alert(' Welcome to the Music Memory Game. In order to win you have to match all of the cards. The timer starts Now!')
+// We start off with an alert box to let the user know the rules of the game
 
-const cards = document.querySelectorAll('.memory-card');
+const cards = document.querySelectorAll('.memory-card'); 
+// storing all of our memory cards in a variable.
 
 let hasFlippedCard = false;
+// variable will be used to determine if card has been flipped.
+
 let lockBoard = false;
+// variable will be used to stop more than two cards from being selected at once
+
 let firstCard, secondCard;
 
+let seconds = 0;
+// variable will be used in our function to count time spent finding matches
 
-
-
+let el = document.getElementById('seconds-counter');
+// Storing div in a variable so we can update time in HTML
 
 function flipCard () {
   //Makes it to were only two cards can be selected at a time
@@ -41,11 +49,28 @@ function checkForMatch(){
     if (firstCard.dataset.framework ===
       secondCard.dataset.framework) {
       disableCards();
+
+    if (document.querySelectorAll('.flip').length ===
+        12) {
+
+        setTimeout (function() { 
+        alert('You are a musical JEEN-YUHS! It only took ' + el.innerText+ ' for you to find all of the matches.');
+        location.reload();
+      }, 700);
+        
+      }
+
+
     }
+    // Our function to check for matches and to check and see if all the matches have been found.
+    //  Which will trigger the alert message to end the game and refresh the page. 
+
     
     else{
   //What if it is not a match? 
 unFlipCards();
+
+
   }
 }
 
@@ -72,123 +97,18 @@ setTimeout(() => {
     card.style.order = randomPos;
   });
 })()
+// function to keep the cards random every time the page is refreshed.
+
+function incrementSeconds() {
+    seconds += 1;
+    el.innerText = seconds + " seconds";
+}
+
+const cancel = setInterval(incrementSeconds, 1000);
+
+//Count down logic. Updates the time in second info is used to tell user how long it took them 
+// to find all the matches.
+
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-
-//timer test
-
-// Credit: Mateusz Rybczonec
-
-const FULL_DASH_ARRAY = 283;
-const WARNING_THRESHOLD = 10;
-const ALERT_THRESHOLD = 5;
-
-const COLOR_CODES = {
-  info: {
-    color: "green"
-  },
-  warning: {
-    color: "orange",
-    threshold: WARNING_THRESHOLD
-  },
-  alert: {
-    color: "red",
-    threshold: ALERT_THRESHOLD
-  }
-};
-
-const TIME_LIMIT = 20;
-let timePassed = 0;
-let timeLeft = TIME_LIMIT;
-let timerInterval = null;
-let remainingPathColor = COLOR_CODES.info.color;
-
-document.getElementById("app").innerHTML = `
-<div class="base-timer">
-  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-    <g class="base-timer__circle">
-      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
-      <path
-        id="base-timer-path-remaining"
-        stroke-dasharray="283"
-        class="base-timer__path-remaining ${remainingPathColor}"
-        d="
-          M 50, 50
-          m -45, 0
-          a 45,45 0 1,0 90,0
-          a 45,45 0 1,0 -90,0
-        "
-      ></path>
-    </g>
-  </svg>
-  <span id="base-timer-label" class="base-timer__label">${formatTime(
-    timeLeft
-  )}</span>
-</div>
-`;
-
-startTimer();
-
-function onTimesUp() {
-  clearInterval(timerInterval);
-}
-
-function startTimer() {
-  timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
-    timeLeft = TIME_LIMIT - timePassed;
-    document.getElementById("base-timer-label").innerHTML = formatTime(
-      timeLeft
-    );
-    setCircleDasharray();
-    setRemainingPathColor(timeLeft);
-
-    if (timeLeft === 0) {
-      onTimesUp();
-    }
-  }, 1000);
-}
-
-function formatTime(time) {
-  const minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-
-  if (seconds < 10) {
-    seconds = `0${seconds}`;
-  }
-
-  return `${minutes}:${seconds}`;
-}
-
-function setRemainingPathColor(timeLeft) {
-  const { alert, warning, info } = COLOR_CODES;
-  if (timeLeft <= alert.threshold) {
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.remove(warning.color);
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.add(alert.color);
-  } else if (timeLeft <= warning.threshold) {
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.remove(info.color);
-    document
-      .getElementById("base-timer-path-remaining")
-      .classList.add(warning.color);
-  }
-}
-
-function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-}
-
-function setCircleDasharray() {
-  const circleDasharray = `${(
-    calculateTimeFraction() * FULL_DASH_ARRAY
-  ).toFixed(0)} 283`;
-  document
-    .getElementById("base-timer-path-remaining")
-    .setAttribute("stroke-dasharray", circleDasharray);
-}
+// adds an event listner to every card when it is clicked and runs the flipCard function.
